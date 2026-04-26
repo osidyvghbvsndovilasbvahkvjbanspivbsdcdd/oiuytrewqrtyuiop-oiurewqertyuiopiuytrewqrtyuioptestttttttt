b8d96d95b7857df459767bd8d719a3217f0ad1a0
osidyvghbvsndovilasbvahkvjbanspivbsdcdd <bitpapax@gmail.com>
2026-04-26 11:05:16 +0100

    Delete certificate-system-FINAL-SECURE directory

--- certificate-system-FINAL-SECURE/electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  selectTemplateImage: () => ipcRenderer.invoke('select-template-image'),
-  savePDF: (data) => ipcRenderer.invoke('save-pdf', data),
-  getAppVersion: () => ipcRenderer.invoke('get-app-version'),
-});
